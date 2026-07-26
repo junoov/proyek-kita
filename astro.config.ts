69a8639faf218a2c90c8c9a99f686b8d7ffd6957
junoov <rizkynoviansyah20@gmail.com>
2026-07-26 15:29:20 +0700

    add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import vercel from "@astrojs/vercel"
 
 export default defineConfig({
   integrations: [react()],
-  site: "https://karsa-uniform.example",
+  adapter: vercel(),
+  site: "https://karsa-uniform.vercel.app",
   server: {
     host: true,
   },
