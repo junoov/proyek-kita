13258a71d668d164fccee14762cf749b49bcf9ca
junoov <junoov@users.noreply.github.com>
2026-08-04 20:44:31 +0700

    Fix Cloudflare Pages build configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import cloudflare from "@astrojs/cloudflare"
 
 export default defineConfig({
   integrations: [react()],
+  adapter: cloudflare(),
+  output: "static",
   site: "https://rollercustomize.com",
   server: {
     host: true,
